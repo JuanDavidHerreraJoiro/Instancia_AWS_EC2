719ae2bf795a223c6e9f4d694c16273b7ebea23b
Juan David Herrera Joiro <juandavidherrera@innovati.com.col>
2022-11-10 00:38:11 -0500

    Instancia de AWS EC2

--- Instancia_EC2_1/main.tf
+++ Instancia_EC2_1/main.tf
@@ -1,8 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXN5XSS2XTZNXIDHU"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 data "aws_ami" "ubuntu" {
