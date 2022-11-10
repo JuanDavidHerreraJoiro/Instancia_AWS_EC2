fd0f385c66dd24e45133d5faeda1c9c93f4d78c1
Juan David Herrera Joiro <juandavidherrera@innovati.com.col>
2022-11-10 00:28:56 -0500

    Instancia de AWS EC2

---
+++ Instancia_EC2_1/main.tf
@@ -0,0 +1,35 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAXN5XSS2XTZNXIDHU"
+  secret_key = "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"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "terraform_ec2_1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform EC2-1"
+  }
+}
+
+/*output "public_ip" {
+  value       = aws_instance.web.public_ip
+}*/
